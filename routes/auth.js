bd88904f58898b9551aca42573fb2774a1f300f5
Linus <linus.react.projects@gmail.com>
2020-08-02 13:37:56 +0200

    fixed auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const JsonWebToken = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+
 
 //@route  GET api/auth
 //@desc   get logged in user
@@ -9,11 +15,49 @@ router.get("/",(req,res)=>{
     res.send("Get logged in user");
 });
 
-//@route  GET api/auth
+//@route  POST api/auth
 //@desc   Auth user & get token
 //@access Public
-router.post("/", (req,res)=>{
-   res.send("log in user ")
+router.post("/",
+    [check("email", "Include a valid email address.").isEmail(),
+            check("password", "password is required").exists()],
+    async (req,res) => {
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+                return res.status(400).json({errors:errors.array()})
+        }
+        const {email, password} = req.body;
+        try {
+            // a check to see if the user exists.
+            let user = await User.findOne({email});
+            //if there's no user send backa error message.
+            if(!user){
+                return res.status(400).json({msg:"Invalid Credentials"});
+            }
+            // checks the password given with the password in DB
+            const isMatch = await bcrypt.compare(password,user.password);
+            if(!isMatch){
+                return res.status(400).json({msg:"Invalid Credentials"})
+            }
+
+            // creating the payload ie. the object we want to send in the token.
+            const payload = {
+                user:user.id
+            }
+            JsonWebToken.sign(payload,config.get("jwtSecret"),{expiresIn: 360000},(err, token)=>{
+                if(err){
+                    throw err;
+                }
+                res.json({
+                    token
+                });
+            });
+        }catch (e) {
+            console.error(e.message);
+            res.status(500).send("server error");
+        }
+
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
-const { check,body, validationResult } = require('express-validator');
+const JsonWebToken = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require('express-validator');
 
 //@route  POST api/users
 //@desc   Register a user
@@ -14,29 +15,46 @@ router.post("/",
       check("email","Please include a valid email").isEmail(),
       check("password","Please enter a password with 6 or more characters").isLength({min: 6})],
       async (req,res) => {
-    const errors = validationResult(req);
+
+      const errors = validationResult(req);
+
     if(!errors.isEmpty()){
       return res.status(400).json({errors:errors.array()})
     }
+
    const {name,email,password} = req.body;
+
     try {
       let user = await User.findOne({email});
 
       if(user){
        res.status(400).json({msg:"User already exsist"});
       }
+
       user = new User({
         name,
         email,
         password,
       });
+
       //starting encryption of password..
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password,salt);
       await user.save();
 
-      res.send("User saved");
+      // creating the payload ie. the object we want to send in the token.
+      const payload = {
+        user:user.id
+      }
+      JsonWebToken.sign(payload,config.get("jwtSecret"),{expiresIn: 360000},(err, token)=>{
+        if(err){
+          throw err;
+        }
+        res.json({
+          token
+        });
+      });
     }catch (e) {
       console.error(e.message);
       res.status(500).send("Server error");
