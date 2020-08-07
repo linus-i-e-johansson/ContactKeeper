72bd12820e5e1efd5e1651c58b032c28379ae489
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2020-08-07 15:25:43 +0200

    fixed typo in user.js

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.post("/",
     [check("name", "Name is required").not().isEmpty(),
       check("email","Please include a valid email").isEmail(),
       check("password","Please enter a password with 6 or more characters").isLength({min: 6})],
-      async (req,res) => {
+      async (req, res) => {
 
       const errors = validationResult(req);
 
@@ -22,13 +22,12 @@ router.post("/",
       return res.status(400).json({errors:errors.array()})
     }
 
-   const {name,email,password} = req.body;
-
+    const {name,email,password} = req.body;
     try {
       let user = await User.findOne({email});
 
       if(user){
-       res.status(400).json({msg:"User already exsist"});
+       res.status(400).json({msg:"User already exist"});
       }
 
       user = new User({
