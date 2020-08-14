e62cd67f69d986a808c98868b9c912576a89f321
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2020-08-14 21:19:46 +0200

    added func for auth

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
+import setAuthToken from "../../utils/setAuthToken";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -22,7 +23,20 @@ const AuthState = (props) => {
     error: null,
   };
   const [state, dispatch] = useReducer(authReducer, initialState);
+
   // load user, checks which user is actually logged in.
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.get("/api/auth");
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (e) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
+
   // register user, signs user upp and gets a token back.
   const register = async (formdata) => {
     const config = {
@@ -34,13 +48,20 @@ const AuthState = (props) => {
     try {
       const res = await axios.post("/api/users", formdata, config);
       dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+      loadUser();
     } catch (err) {
       dispatch({ type: REGISTER_FAIL, payload: err.response.data.msg });
     }
   };
-  // ogin user, logs user in and gets the token.
-  // logout, destorys the token.
+  // login user, logs user in and gets the token.
+
+  // logout, destroys the token.
+
   // clear errors, clear out any errors in the state.
+  const clearErrors = () => {
+    dispatch({ type: CLEAR_ERRORS });
+  };
+
   return (
     <AuthContext.Provider
       value={{
@@ -50,6 +71,8 @@ const AuthState = (props) => {
         user: state.user,
         error: state.error,
         register,
+        clearErrors,
+        loadUser,
       }}
     >
       {props.children}
--- routes/users.js
+++ routes/users.js
@@ -5,31 +5,34 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const JsonWebToken = require("jsonwebtoken");
 const config = require("config");
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 
 //@route  POST api/users
 //@desc   Register a user
 //@access Public
 
-router.post("/",
-    [
-      check("name", "Name is required").not().isEmpty(),
-      check("email","Please include a valid email").isEmail(),
-      check("password","Please enter a password with 6 or more characters").isLength({min: 6})
-            ],
-      async (req, res) => {
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-      const errors = validationResult(req);
-        if(!errors.isEmpty()){
-          return res.status(400).json({errors:errors.array()})
-        }
-
-    const {name,email,password} = req.body;
+    const { name, email, password } = req.body;
     try {
-      let user = await User.findOne({email});
+      let user = await User.findOne({ email });
 
-      if(user){
-       res.status(400).json({msg:"User already exist"});
+      if (user) {
+        res.status(400).json({ msg: "User already exist" });
       }
 
       user = new User({
@@ -40,29 +43,30 @@ router.post("/",
 
       //starting encryption of password..
       const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password,salt);
-
+      user.password = await bcrypt.hash(password, salt);
       await user.save();
 
       // creating the payload ie. the object we want to send in the token.
       const payload = {
         user: {
-            id: user.id
-        }
-      }
-        console.log("from user - Post user: "+ payload.user);
+          id: user.id,
+        },
+      };
       // Generation of json token, singing it.
-      JsonWebToken.sign(payload, config.get("jwtSecret"),{expiresIn: 360000},(err, token) => {
-        if(err){
-          throw err;
+      JsonWebToken.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
         }
-          console.log("Token singing successfull: ",token)
-        res.json({ token });
-      });
-    }catch (e) {
-      console.error(e.message);
+      );
+    } catch (e) {
       res.status(500).send("Server error");
     }
-});
-
+  }
+);
 module.exports = router;
