db6ffdd2a896325d8eafbbe3a3e2040e465d5b63
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2020-08-15 12:42:50 +0200

    fixed what a logged in user sees and what a guest sees.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,37 +1,51 @@
-import React from 'react';
+import React, { Fragment, useContext } from "react";
+import AuthContext from "../../context/auth/authContext";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Navbar = ({title,icon}) => {
-    return (
-        <div className={"navbar bg-primary"}>
-            <h1>
-                <i className={icon} />{title}
-            </h1>
-            <ul>
-                <li>
-                    <Link to={"/"}>Home</Link>
-                </li>
-                <li>
-                    <Link to={"/about"}>About</Link>
-                </li>
-                <li>
-                    <Link to={"/register"}>Register</Link>
-                </li>
-                <li>
-                    <Link to={"/login"}>Login</Link>
-                </li>
-            </ul>
-        </div>
-    );
+const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
+  const authLinks = (
+    <Fragment>
+      <li> Hello {user && user.name}</li>
+      <li>
+        <a href="#">
+          <i className={"fas fa-sign-out-alt"}></i>
+          <span className={"hide-sm"}> Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to={"/register"}>Register</Link>
+      </li>
+      <li>
+        <Link to={"/login"}>Login</Link>
+      </li>
+    </Fragment>
+  );
+
+  return (
+    <div className={"navbar bg-primary"}>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
 };
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
 Navbar.defaultProps = {
-    title:"ContactKeeper",
-    icon:"fas fa-id-card-alt"
-}
+  title: "ContactKeeper",
+  icon: "fas fa-id-card-alt",
+};
 
 export default Navbar;
